0a080f3ff8f796b3c0cf7a56faad3344959644a6
Samuel Santos <mucasantos@gmail.com>
2024-06-06 20:41:04 -0300

    adjust buttons

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/services/app_constants.dart';
-import 'package:grocery_app/views/widgets/app_button.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -10,7 +9,7 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
-  var items = List<String>.generate(10000, (i) => 'Item $i');
+  var items = List<String>.generate(10, (i) => 'Item $i');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,51 +25,78 @@ class _CartScreenState extends State<CartScreen> {
           return Container(
             padding: const EdgeInsets.all(8),
             margin: const EdgeInsets.all(2),
-            color: Colors.blueGrey,
+            color: const Color.fromARGB(35, 83, 177, 117),
             width: double.infinity,
             height: 100,
-            child: Row(children: [
-              Image.asset(
-                "assets/images/start.jpg",
-              ),
-              Column(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text("data"),
-                  const Text("data"),
-                  Row(
+                  Image.asset(
+                    "assets/images/start.jpg",
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      SizedBox(
-                          width: 40,
-                          height: 40,
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: mainColor,
-                                  foregroundColor: Colors.white,
-                                  shape: const RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(
-                                    Radius.circular(10),
-                                  ))),
-                              child: const Icon(Icons.remove),
-                              onPressed: () {})),
-                      const Text("1"),
-                      SizedBox(
-                          width: 40,
-                          height: 40,
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                  backgroundColor: mainColor,
-                                  foregroundColor: Colors.white,
-                                  shape: const RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(
-                                    Radius.circular(10),
-                                  ))),
-                              child: const Icon(Icons.add),
-                              onPressed: () {}))
+                      const Text("data"),
+                      const Text("data"),
+                      Row(
+                        children: [
+                          Container(
+                            width: 30,
+                            height: 30,
+                            //  margin: const EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                                color: mainColor,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: IconButton(
+                              iconSize: 20,
+                              padding: EdgeInsets.zero,
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.remove,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text("1"),
+                          ),
+                          Container(
+                            width: 30,
+                            height: 30,
+                            //  margin: const EdgeInsets.all(16),
+                            decoration: BoxDecoration(
+                                color: mainColor,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: IconButton(
+                              iconSize: 20,
+                              padding: EdgeInsets.zero,
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Icon(Icons.close),
+                      Text(
+                        "R\$4.99",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
                     ],
                   )
-                ],
-              )
-            ]),
+                ]),
           );
         },
       ),
